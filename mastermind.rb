629307f0767a64bdfcab8b6c3334c88df243621d
nbrennan26 <nbrennan26@gmail.com>
2022-07-27 15:41:48 -0500

    rework player creation to utilize a hash to store players in game, and individual Classes to distinguish human/computer & maker/breaker

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-# Main Class with most game-driving logic
-class Game
-  def initialize
-    # Initiate Code
-  end
-end
-
 # Codemaker Module
 module Codemaker
   def create_sequence
@@ -29,39 +22,75 @@ module Codebreaker
   end
 end
 
+# Main Class with most game-driving logic
+class Game
+  attr_reader :players, :p1_role
+
+  def initialize
+    puts ' '
+    puts 'PLAYER ONE, Would you like to play as the CODEMAKER or the CODEBREAKER?'
+    puts "Press 'm' for CODEMAKER or 'b' for CODEBREAKER"
+    @p1_role = gets.chomp
+    puts ' '
+    puts 'Would you like to play against another PLAYER, or the COMPUTER?'
+    puts "Press 'p' for PLAYER or 'c' for COMPUTER"
+    @p2_player = gets.chomp
+    @players = {}
+  end
+
+  def create_roles
+    if @p1_role == 'b'
+      player_one = HumanBreaker.new(self, 1)
+      @players[:breaker] = player_one
+      player_two = @p2_player == 'c' ? ComputerMaker.new(self, 2) : HumanMaker.new(self, 2)
+      @players[:maker] = player_two
+    else
+      player_one = HumanMaker.new(self, 1)
+      @players[:maker] = player_one
+      player_two = @p2_player == 'c' ? ComputerBreaker.new(self, 2) : HumanBreaker.new(self, 2)
+      @players[:breaker] = player_two
+    end
+  end
+
+  def reveal_players
+    p @players
+  end
+end
+
 # Player Superclass
 class Player
   attr_reader :role
 
-  def initialize
-    puts 'Would you like to play as the CODEMAKER or the CODEBREAKER?'
-    puts "press 'm' for CODEMAKER or 'b' for CODEBREAKER"
-    @role = gets.chomp
-    puts @role
+  def initialize(game, player_no)
+    @game = game
+    @player_no = player_no
   end
-
-  @role == 'm' ? (include Codemaker) : (include Codebreaker)
-
-  # if @role == 'b'
-  #   include Codebreaker
-  # else
-  #   include Codemaker
-  # end
 end
 
-# Human Player Subclass
-class HumanPlayer < Player; end
+# Human Codebreaker Subclass
+class HumanBreaker < Player
+  include Codebreaker
+end
 
-# Computer Player Subclass
-class ComputerPlayer < Player; end
+# Human Codemaker Subclass
+class HumanMaker < Player
+  include Codemaker
+end
+
+# Computer Codebreaker Subclass
+class ComputerBreaker < Player
+  include Codebreaker
+end
+
+# Computer Codemaker Subclass
+class ComputerMaker < Player
+  include Codemaker
+end
 
 # Play commands
-human = HumanPlayer.new
-com = ComputerPlayer.new
+game = Game.new
+game.create_roles
+game.reveal_players
 
-puts "Human Role: #{human.role}"
-puts "Computer Role: #{com.role}"
-
-human.test_breaker
-com.test_breaker
-com.test_maker
+game.players[:breaker].test_breaker
+game.players[:maker].test_maker
