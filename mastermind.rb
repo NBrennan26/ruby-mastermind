98b36461b7aa49f1b019cbd184dbc7e5bb20f376
nbrennan26 <nbrennan26@gmail.com>
2022-07-28 13:41:36 -0500

    Refactor to include files for player classes module and game text module

--- mastermind.rb
+++ mastermind.rb
@@ -1,39 +1,19 @@
 # frozen_string_literal: true
 
-# Codemaker Module
-module Codemaker
-  def create_sequence
-    # create four-number code
-  end
-
-  def test_maker
-    puts 'Maker included'
-  end
-end
-
-# Codebreaker Module
-module Codebreaker
-  def guess_code
-    # gets input from user (or random for AI?)
-  end
-
-  def test_breaker
-    puts 'Breaker included'
-  end
-end
+require_relative 'game_classes'
+require_relative 'game_text'
 
 # Main Class with most game-driving logic
 class Game
+  include GameClasses
+  include GameText
+
   attr_reader :players, :p1_role
 
   def initialize
-    puts ' '
-    puts 'PLAYER ONE, Would you like to play as the CODEMAKER or the CODEBREAKER?'
-    puts "Press 'm' for CODEMAKER or 'b' for CODEBREAKER"
+    prompt_player_role
     @p1_role = gets.chomp
-    puts ' '
-    puts 'Would you like to play against another PLAYER, or the COMPUTER?'
-    puts "Press 'p' for PLAYER or 'c' for COMPUTER"
+    prompt_player_two_class
     @p2_player = gets.chomp
     @players = {}
   end
@@ -55,36 +35,6 @@ class Game
   def reveal_players
     p @players
   end
-end
-
-# Player Superclass
-class Player
-  attr_reader :role
-
-  def initialize(game, player_no)
-    @game = game
-    @player_no = player_no
-  end
-end
-
-# Human Codebreaker Subclass
-class HumanBreaker < Player
-  include Codebreaker
-end
-
-# Human Codemaker Subclass
-class HumanMaker < Player
-  include Codemaker
-end
-
-# Computer Codebreaker Subclass
-class ComputerBreaker < Player
-  include Codebreaker
-end
-
-# Computer Codemaker Subclass
-class ComputerMaker < Player
-  include Codemaker
 end
 
 # Play commands
